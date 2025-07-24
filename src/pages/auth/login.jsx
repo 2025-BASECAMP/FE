5f1550b954a0ede30d624800c30827015ed4d8a6
gpdnjs8 <jade3735@naver.com>
2025-07-24 16:02:05 +0900

    ♻️ refactor: 파일 삭제

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import * as S from "../../styles/auth/login.style";
 
-const login = () => {
-  return (
-    <S.LoginWrapper>
-      <S.LoginBox>
-        <S.Title>로그인</S.Title>
-        <S.Input type="text" placeholder="이메일" />
-        <S.Input type="password" placeholder="비밀번호" />
-        <S.LoginButton>로그인</S.LoginButton>
-      </S.LoginBox>
-    </S.LoginWrapper>
-  );
-};
+function login() {
+  return <div>login</div>;
+}
 
 export default login;
