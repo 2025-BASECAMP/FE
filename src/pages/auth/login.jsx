3323f40888380ade22c442daaa81eaa716fd031d
gpdnjs8 <jade3735@naver.com>
2025-09-13 16:54:14 +0900

    ✨ feat: 로그인 페이지 구현

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as S from "../../styles/auth/login.style";
 
-function login() {
-  return <div>login</div>;
+export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/home");
+  };
+
+  return (
+    <S.LoginContainer>
+      <S.LoginCard>
+        <S.LoginTitle>로그인</S.LoginTitle>
+
+        <S.LoginForm onSubmit={handleSubmit}>
+          <S.FormGroup>
+            <label>아이디</label>
+            <input
+              type="id"
+              placeholder=""
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </S.FormGroup>
+
+          <S.FormGroup>
+            <label>비밀번호</label>
+            <input
+              type="password"
+              placeholder=""
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </S.FormGroup>
+
+          <S.LoginBtn type="submit">로그인</S.LoginBtn>
+        </S.LoginForm>
+
+        <S.SignupText>
+          계정이 없으신가요?{" "}
+          <S.SignupLink href="/signup">회원가입</S.SignupLink>
+        </S.SignupText>
+      </S.LoginCard>
+    </S.LoginContainer>
+  );
 }
-
-export default login;
